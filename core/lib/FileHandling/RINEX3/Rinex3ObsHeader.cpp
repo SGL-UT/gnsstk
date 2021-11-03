d8f9f46db3d09bd54b1dc6270a458fa2fa09e561
Brian Tolman <btolman@arlut.utexas.edu>
2021-11-03 11:14:33 -0500

    allow RINEX 3.05 even though no changes made

--- core/lib/FileHandling/RINEX3/Rinex3ObsHeader.cpp
+++ core/lib/FileHandling/RINEX3/Rinex3ObsHeader.cpp
@@ -2683,6 +2683,7 @@ namespace gpstk
       else if(version < 3.03) return allValid302;
       else if(version < 3.04) return allValid303;
       else if(version < 3.05) return allValid303;
+      else if(version < 3.06) return allValid303;
       return Fields();
    }
 
