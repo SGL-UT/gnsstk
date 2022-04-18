baca4c203fe19992a245b18ca7ca25f5222aa2ef
johnk <johnk@arlut.utexas.edu>
2022-04-18 17:43:58 -0500

    Fix RPATH to be platform independent

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -108,16 +108,16 @@ if( USE_RPATH )
     # (but later on when installing)
     set( CMAKE_BUILD_WITH_INSTALL_RPATH FALSE )
 
-    set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../lib" )
+    set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../${CMAKE_INSTALL_LIBDIR}" )
 
     # Add the automatically determined parts of the RPATH
     # which point to directories outside the build tree to the install RPATH
     set( CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE )
 
     # The RPATH to be used when installing, but only if it's not a system directory
-    list( FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../lib" isSystemDir )
+    list( FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../${CMAKE_INSTALL_LIBDIR}" isSystemDir )
     if( "${isSystemDir}" STREQUAL "-1" )
-       set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../lib" )
+       set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../${CMAKE_INSTALL_LIBDIR}" )
     endif( "${isSystemDir}" STREQUAL "-1" )
 else()
     set( CMAKE_SKIP_BUILD_RPATH TRUE )
