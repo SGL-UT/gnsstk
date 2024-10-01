d992084a89cc103a23e5d2dd2d304ba852e2316d
Benji Hillard <bhillard@arlut.utexas.edu>
2024-10-01 15:00:24 -0500

    ObsEpochMap test coverage

--- core/tests/ClockModel/ObsEpochMap_T.cpp
+++ core/tests/ClockModel/ObsEpochMap_T.cpp
@@ -36,18 +36,94 @@
 //
 //==============================================================================
 
+#include "SvObsEpoch.hpp"
+#include "ObsEpochMap.hpp"
 #include "TestUtil.hpp"
 #include <iostream>
+
+using namespace std;
+using namespace gnsstk;
 
 class ObsEpochMap_T
 {
         public:
 		ObsEpochMap_T(){}// Default Constructor, set the precision value
-		~ObsEpochMap_T() {} // Default Desructor
+		~ObsEpochMap_T() {} // Default Destructor
+		int test_stream();
 };
+
+int ObsEpochMap_T::test_stream() 
+{
+	TUDEF("ObsEpochMap", "ostream");
+	SvObsEpoch svObsEpoch;
+
+	string expectEmptyOutput = "";
+	ostringstream emptyOutput;
+    emptyOutput << svObsEpoch;
+
+	TUASSERTE(string, expectEmptyOutput, emptyOutput.str());
+
+    SatID SV1 = SatID(1, SatelliteSystem::GPS);
+    SatID SV2 = SatID(2, SatelliteSystem::GPS);
+    SatID SV3 = SatID(3, SatelliteSystem::GPS);
+
+	ObsID ObsId1;
+	ObsID ObsId2;
+	ObsID ObsId3;
+
+	SvObsEpoch svObsEpoch1;
+	svObsEpoch1.insert(make_pair(ObsId1, 25.22));
+	svObsEpoch1.insert(make_pair(ObsId2, 37.24));
+	svObsEpoch1.insert(make_pair(ObsId3, 48.00));
+	svObsEpoch1.svid = SV1;
+	svObsEpoch1.azimuth = 10;
+	svObsEpoch1.elevation = 10;
+
+	SvObsEpoch svObsEpoch2;
+	svObsEpoch1.insert(make_pair(ObsId1, 11.3));
+	svObsEpoch1.insert(make_pair(ObsId2, 32.4));
+	svObsEpoch1.insert(make_pair(ObsId3, 19.45));
+	svObsEpoch1.svid = SV2;
+	svObsEpoch1.azimuth = 12;
+	svObsEpoch1.elevation = 12;
+
+	SvObsEpoch svObsEpoch3;
+	svObsEpoch1.insert(make_pair(ObsId1, 20.43));
+	svObsEpoch1.insert(make_pair(ObsId2, 17.32));
+	svObsEpoch1.insert(make_pair(ObsId3, 31.3));
+	svObsEpoch1.svid = SV3;
+	svObsEpoch1.azimuth = 15;
+	svObsEpoch1.elevation = 15;
+
+	ObsEpoch oe;
+	oe.insert(make_pair(SV1, svObsEpoch1));
+	oe.insert(make_pair(SV2, svObsEpoch2));
+	oe.insert(make_pair(SV3, svObsEpoch3));
+	CommonTime time;
+	oe.time = time;
+	oe.rxClock = vdouble(25.25);
+
+	string expectFullOutput = "0000000 00000000 0.000000000000000 UNK, rxClock: 25.25\n"
+							  "GPS 1: UnknownBand UnknownCode UnknownType: 25.22\n"
+							  "GPS 2: \n"
+							  "GPS 3: \n";
+	ostringstream fullOutput;
+    fullOutput << oe;
+
+	TUASSERTE(string, expectFullOutput, fullOutput.str());
+	
+	TURETURN();
+}
 
 
 int main() //Main function to initialize and run all tests above
 {
-	return 0; //Return the total number of errors
+	   int errorTotal = 0;
+
+   		ObsEpochMap_T testClass;
+   
+		errorTotal += testClass.test_stream();
+
+		cout << "Total Failures for " << __FILE__ << ": " << errorTotal << endl; 
+		return errorTotal;
 }
